b2149c4cf2c7027cafde4679d3ec97173f9bb8ed
Grecx <aragour99@gmail.com>
2023-06-18 16:32:53 -0700

    ptintf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : using main function to print a string
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art);
+	return (0);
+
+}
