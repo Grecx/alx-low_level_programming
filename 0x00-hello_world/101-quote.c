6e571d822c4b2f4cfe813fb5a2868850604fbbbb
Grecx <aragour99@gmail.com>
2023-06-18 18:51:35 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Discription: using write function to standard error
+ *
+ * Return: 1
+*/
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	return (1);
+}
