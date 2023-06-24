9144ffca9e96d9700d7ffecd375d5f437864218b
Grecx <aragour99@gmail.com>
2023-06-23 17:20:06 -0700

    number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,14 +5,20 @@
  */
 void more_numbers(void)
 {
-	int num, count;
+	int num, count, row;
 
-	for (count = 0; count <= 10; count++)
+	for (row = 1; row <= 10; row++)
 	{
-		for (num = 0; num <= 14; num++)
+		for (count = 0; count <= 14; count++)
 		{
+			num = count;
 			if (num > 9)
-				_putchar((num / 10) + '0');
-			_putchar((num % 10) + '0');
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
 		}
 		_putchar('\n');
+	}
+}
