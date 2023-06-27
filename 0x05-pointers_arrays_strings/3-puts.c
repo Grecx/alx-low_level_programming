c5b8c243c03510d6eb8befaa2b68da434712e6e5
Grecx <aragour99@gmail.com>
2023-06-26 17:36:08 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,10 @@
 
 #include "main.h"
 /**
- * _puts - prints a string, followed by a new line, to stdout
+ * _putchar - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
-void _puts(char *str)
+void _putchar(char *str)
 {
 	while (*str != '\0')
 	{
