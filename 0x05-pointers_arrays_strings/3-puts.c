5c4cf18a65e7d51cd8eecaabb292796bc394b1ee
Grecx <aragour99@gmail.com>
2023-06-26 17:01:40 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string to stdout.
+ * _puts - Prints a string
  * @str: The string to be printed.
+ * Return: Nothing
  */
 void _puts(char *str)
 {
 	while (*str)
-		_putchar(str++);
+	{	_putchar(*str + 0);
+		str++;
+	}
 
 	_putchar('\n');
 }
