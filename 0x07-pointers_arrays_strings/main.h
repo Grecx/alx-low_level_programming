8d8b61e21a443d29da128e1e28e7eaf63b91b31c
Grecx <aragour99@gmail.com>
2023-07-03 12:57:12 -0700

    memeset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Auth: Amos Mwongela 
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
+
+#include <stddef.h>
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 void *_memset(void *s, int c, size_t n);
