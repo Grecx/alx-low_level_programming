48f5bd4c97b765f5ee49964eeecf0b95adafe6fe
Grecx <aragour99@gmail.com>
2023-07-03 16:38:18 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - main
+ * @s: The string to be searched.
+ * @accept: The prefix to be measured.
+ *
+ * Return: b
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int b = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				b++;
+				break;
+			}
+
+			else if (accept[i + 1] == '\0')
+				return (b);
+		}
+
+		s++;
+	}
+
+	return (bytes);
+}
