809aca3f8dfa42f6ff396a38448392bc3ead77ed
Grecx <aragour99@gmail.com>
2023-07-03 13:06:12 -0700

    memeset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,11 @@
  */
 void *_memset(void *s, int c, size_t n)
 {
-	unsigned int index;
+	unsigned int intel;
 	unsigned char *memory = s, value = c;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
+	for (intel = 0; intel < n; intel++)
+		memory[intel] = value;
 
 	return (memory);
 }
